b377dde9a239b1623999415d70f1fceda18c435e
Cassiopeia316 <dvinagunawan@gmail.com>
2023-04-17 22:56:16 +0700

    EnumUser (upper to lower), add column & check pass

--- backend/models/users.py
+++ backend/models/users.py
@@ -2,11 +2,11 @@ from sqlalchemy.dialects.postgresql import UUID
 import uuid
 import enum
 
-from server import db
+from server import db, bcrypt
 
 class EnumUsersRole(enum.Enum):
-    ADMIN = "admin"
-    BUYER = "buyer"
+    admin = "admin"
+    buyer = "buyer"
 
 class User(db.Model):
     __tablename__ = 'users' #untuk mengganti judul / nama tabel
@@ -15,11 +15,16 @@ class User(db.Model):
     name = db.Column(db.String)
     email = db.Column(db.String)
     role = db.Column(db.Enum(EnumUsersRole))
+    password = db.Column(db.String)
 
-    def __init__(self, name, email, role):
+    def __init__(self, name, email, role, password):
         self.name = name
         self.email = email
         self.role = role
+        self.password = bcrypt.generate_password_hash(password, 10).decode('utf-8')
 
     # def __repr__(self):
     #     return '<id {}>'.format(self.id)
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password) # returns True
