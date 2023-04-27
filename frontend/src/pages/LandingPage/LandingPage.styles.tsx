8a7d9e95f3786aa8cf5ca753bcf3041ab9a02dc8
Cassiopeia316 <dvinagunawan@gmail.com>
2023-04-27 12:53:46 +0700

    landing page public

--- frontend/src/pages/LandingPage/LandingPage.styles.tsx
+++ frontend/src/pages/LandingPage/LandingPage.styles.tsx
@@ -1,90 +1,99 @@
-import { Form } from "react-bootstrap";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Button from "@components/Button";
+import { Card } from "react-bootstrap";
 
 export const Wrapper = styled.div`
-
-`
-
-export const BackgroundImage = styled.img`
-    position: fixed;
-    inset: 0;
-    width: 100vw;
-    height: 100vh;
-    object-fit: cover;
-    z-index: -1;   
-`
-
-export const SignInLogo = styled.img`
-    margin-top: -50px;
-    width: 100px;
-    height: 100px;
-    margin-left: calc(50% - 50px);
-`
-
-export const SupermarketLogo = styled.img`
-    margin-top: 10px;
+    /* height: 100vh; */
+    /* overflow: hidden; */
 `
 
 export const Content = styled.div`
-    /* width: 448px;
-    height: 486px;
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 15px;
-    border: 4px solid white;
     display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    padding-bottom: 20px; */
+    /* flex-direction: column; */
+    justify-content: center;
+    margin-top: calc(75px + 35px);
+    /* margin-bottom: 35px; */
+    position: relative;
+    
+    img{
+        width: 65%;
+    }
 `
 
 export const Title = styled.div`
+    position: absolute;
     color: white;
     text-align: center;
-    /* margin: 15px 0; */
+    margin-top: 35px;
+    h2, h4, h6{
+        font-weight: 800;
+        /* font-size: 1.3vw !important; */
+    }
+    h6{
+        margin: 30px 0px;
+    }
 `
 
-export const SubmitButton = styled(Button)`
-    background-color: #F5911D !important;
-    color: var(--white-color) !important;
+export const ViewProduct = styled(Link)`
+    text-align: center;
+    font-size: 14px;
+    padding: 10px 15px;
+    justify-content: center;
+    background-color: #ffffff;
+    opacity: 0.8;
+    color: #555E11 !important;
     box-shadow: none !important;
-    margin: 0 auto;
-    width: 122px;
-    height: 48px;
-    border-radius: 7px;
+    border-radius: 8px;
     border: none;
-    :hover{
-        background-color: #724919 !important;
-    }
-    :active{
-        background-color: #a36319 !important;
+    &:hover{
+        background-color: #959b60 !important;
+        color: #FFFFFF !important;
     }
 `
 
-export const ForgotPassword = styled(Link)`
-    color: white !important;
-    font-size: 14px;
-    width: 85%;
+export const CardContent = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
     margin: 0 auto;
-    /* margin-bottom: 20px; */
-    :hover{
-        color: #9b703f !important;
-        text-decoration: underline !important;
-    }
-    :active{
-        color: #F5911D !important;
-    }
+    margin-top: -50px;
+    margin-bottom: 40px;
+    width: 65%;
 `
-export const DontHaveAnAccount = styled(Link)`
-    color: white !important;
-    font-size: 14px;
-    margin: 0 auto;
-    :hover{
-        color: #9b703f !important;
-        text-decoration: underline !important;
+export const LandingPageCard = styled(Card)`
+    width: 18rem;
+    height: 180px;
+    background-color: transparent;
+    border: none;
+    color: #ffffff;
+    text-align: center;
+    font-size: 20px;
+    i{
+        font-size: 2.5rem;
     }
-    :active{
-        color: #F5911D !important;
+
+    .cardone{
+        background-color: rgb(242, 124, 84, 0.85) !important;
+        border-radius: 8px;
+        border: solid 3px #F4A085;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .cardtwo{
+        background-color: rgb(245, 183, 34, 0.85) !important;
+        border-radius: 8px;
+        border: solid 3px #F4CE71;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .cardthree{
+        background-color: rgb(153, 170, 28, 0.85) !important;
+        border-radius: 8px;
+        border: solid 3px #C3C773;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
 `
--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,18 +1,51 @@
 import React from 'react'
-import { Content, ForgotPassword, Title, Wrapper, BackgroundImage, SignInLogo, SubmitButton, DontHaveAnAccount, SupermarketLogo } from '../LandingPage/LandingPage.styles'
-import Image from '../../assets/images/loginbackground.jpg'
-import UserIcon from '../../assets/images/usericon.png'
-import supermarketlogo from '../../assets/images/supermarketlogo.png'
-
-import InputField  from '@components/Input'
-import routes from '@constants/routes/admin'
+import { Content, Title, CardContent, Wrapper, ViewProduct, LandingPageCard } from './LandingPage.styles'
+import NavbarPublic from '@components/NavbarPublic'
+import Footer from '@components/FooterPublic'
+import Image from '@assets/images/landingpage.png'
+import { routes } from '@constants/route'
+import Card from 'react-bootstrap/Card';
 
 const LandingPage: React.FC = () => {
     return (
         <Wrapper>  
+            <NavbarPublic/>
             <Content>
-                
+                <img src={Image} className="img-fluid" alt="Responsive image"/>
+                <Title>
+                   <h4>WELCOME TO</h4> 
+                   <h2>SUPERMARKET!</h2>
+                   <h6>We're here to serve only the best products for you!</h6>
+                   <ViewProduct to={routes.PRODUCTLIST_PAGE}><i className="fa-solid fa-list"></i> View Product</ViewProduct>
+                </Title>
             </Content>
+            <CardContent>
+                <LandingPageCard >
+                    <Card.Body className='cardone'>
+                        <Card.Title><i className="fa-solid fa-bag-shopping"></i></Card.Title>
+                        <Card.Text>
+                            Always stocked for your needs!
+                        </Card.Text>
+                    </Card.Body>
+                </LandingPageCard>
+                <LandingPageCard >
+                    <Card.Body className='cardtwo'>
+                        <Card.Title><i className="fa-solid fa-house"></i></Card.Title>
+                        <Card.Text>
+                            Home of family essentials!
+                        </Card.Text>
+                    </Card.Body>
+                </LandingPageCard>
+                <LandingPageCard >
+                    <Card.Body className='cardthree'>
+                        <Card.Title><i className="fa-solid fa-cart-shopping"></i></Card.Title>
+                        <Card.Text>
+                            Serving fresh ingredients!
+                        </Card.Text>
+                    </Card.Body>
+                </LandingPageCard>
+            </CardContent>
+            <Footer/>
         </Wrapper>
     )
 }
