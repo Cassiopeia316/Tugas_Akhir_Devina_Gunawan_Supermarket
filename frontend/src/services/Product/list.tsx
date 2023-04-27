a90f26b70a1bd080c29806912d42216d0dca4577
Cassiopeia316 <dvinagunawan@gmail.com>
2023-04-27 13:12:18 +0700

    get product list service

---
+++ frontend/src/services/Product/list.tsx
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import api from "@config/config"
+import { statusActions } from "@store/status"
+import { endpoints } from "@constants/endpoint"
+import UrlUtils from "@utils/urlHelper"
+// import authHeader from "@services/authHeader"
+import handleError from "@services/errorHandling"
+// import { generatePath } from "react-router-dom"
+import { Product } from "@models/product"
+import { Pagination } from "@models/pagination"
+
+interface Request {
+    offset: number
+    limit: number
+}
+
+export type Response = {
+    data: Product[]
+// }
+} & Pagination
+
+export const GetProductListService = createAsyncThunk(
+    endpoints.GETPRODUCTLIST,
+    async (request: Request, { dispatch }) => {
+        try {
+            dispatch(statusActions.setLoading(true))
+
+            const response = await api.get(
+                // generatePath(endpoints.GETPRODUCTLIST)
+                endpoints.GETPRODUCTLIST + "?" + UrlUtils.objectToQueryString(request),
+                // authHeader('application/json')
+            )
+
+            // dispatch(statusActions.setLoading(false))
+
+            const data = (response.data as Response)
+            return data
+        } catch (err) {
+            handleError(err, dispatch)
+        }
+    }
+)
